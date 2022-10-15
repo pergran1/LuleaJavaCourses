d1325a625cc327e411b993991557b17915af5366
pergran1 <pergranberg@hotmail.com>
2022-10-15 17:19:40 +0200

    Added thirdTask in introduction

--- src/IntroductionProgramming/thirdTask.java
+++ src/IntroductionProgramming/thirdTask.java
@@ -3,27 +3,49 @@ package IntroductionProgramming;
 import java.util.Random;
 import java.util.Scanner;
 
+import static java.lang.System.exit;
+import static java.lang.System.in;
+
 public class thirdTask {
 
     public static void main(String args[])
     {
-        Scanner input = new Scanner(System.in);
+        Scanner userInput = new Scanner(System.in);
 
         System.out.println("Välkommen till spelet 12. Du ska slå 1-3 tärningar och försöka få summan 12... ");
 
         int diceOne = 0;
         int diceTwo = 0;
         int diceThree = 0;
+        int sum = 0;
+        int wins = 0;
+        int losses = 0;
+
+        int loopCount = 0;
 
         while(true)
         {
             // ha en counter variabel här som räknar hur många gånger man har spelat och avlutar efter tredje?
+
             System.out.print("Ange vilken tärning du vill slå [1,2,3](avsluta med q): ");
 
-            if(input.hasNextInt())
+
+            String strInput = userInput.nextLine();
+
+            // true = the input is a int
+            Boolean strOrInt = checkInputStrOrInt(strInput);
+
+
+
+            if(strOrInt == true)
             {
-                int intAnswer = input.nextInt();
-                if(intAnswer == 1){
+                int intAnswer = Integer.parseInt(strInput);
+
+                if(intAnswer < 0 || intAnswer > 3){
+                    System.out.println("Du skrev ett nummer som inte är mellan 1 och 3 försök igen tack");
+                    continue;
+                }
+                else if(intAnswer == 1){
                     diceOne = getRandomNumber();
                 }
                 else if(intAnswer == 2) {
@@ -31,33 +53,36 @@ public class thirdTask {
                 }
                 else if(intAnswer == 3) {
                     diceThree = getRandomNumber();
-                } else {
-                    System.out.println("Du skrev ett nummer som inte är mellan 1 och 3 försök igen tack");
+                }
+
+                sum = diceOne + diceTwo + diceThree;
+                loopCount++;
+
+                if(sum == 12){
+                    wins++;
+                    loopCount = 3;
+                }
+
+                if (loopCount == 3){
+                    //start a new game
+                    if(sum > 12){
+                        losses++;
+                    }
+
+                    printResults( diceOne, diceTwo, diceThree, wins, losses);
+                    System.out.println("nästa omgång!");
+                    diceOne = 0;
+                    diceTwo = 0;
+                    diceThree = 0;
+                    loopCount = 0;
                     continue;
                 }
-                printResults( diceOne, diceTwo, diceThree);
+                printResults(diceOne, diceTwo, diceThree, wins, losses);
                 continue;
             }
 
+            exitLoop(strInput);
 
-            // Control if the user wants to quit
-            if(input.hasNext())
-            {
-                String strAnswer = input.nextLine().toLowerCase();
-                if(strAnswer.equals("q")){
-                    System.out.println("Avslutar spel!");
-                    break;
-                } else{
-                    System.out.println("Du angav ett värde som inte har en funktionen i detta spel, vänligen försök igen");
-                }
-                printResults(diceOne, diceTwo, diceThree);
-                continue;
-
-            }
-
-
-
-            input.nextLine();
         }
     }
 
@@ -66,8 +91,28 @@ public class thirdTask {
         return randomObj.nextInt(6) + 1;
     }
 
-    public static void printResults(int diceOne, int diceTwo, int diceThree){
+    public static void printResults(int diceOne, int diceTwo, int diceThree, int wins, int losses){
         int sum = diceOne + diceTwo + diceThree;
-        System.out.printf("%d %d %d sum: %d  #vinst: 0 #förlust: 0 ", diceOne, diceTwo, diceThree, sum);
+        System.out.printf("%d %d %d sum: %d  #vinst: %d #förlust: %d \n", diceOne, diceTwo, diceThree, sum, wins, losses);
+    }
+
+    public static void exitLoop(String str){
+        if (str.toLowerCase().equals("q")){
+            System.out.println("Avslutar spel!");
+            System.exit(0);
+        } else {
+            System.out.println("Du skrev ett kommando som inte finns med, försök igen");
+        }
+    }
+
+    public static boolean checkInputStrOrInt(String str){
+
+        try {
+            Integer.parseInt(str);
+            return true;
+        }
+        catch (NumberFormatException e){
+            return false;
+        }
     }
 }
