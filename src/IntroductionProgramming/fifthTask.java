0e7d4454e8d600aa80489c2e9ecf178287e58ae5
pergran1 <pergranberg@hotmail.com>
2022-12-04 14:42:08 +0100

    Fifth task changed

--- src/IntroductionProgramming/fifthTask.java
+++ src/IntroductionProgramming/fifthTask.java
@@ -2,14 +2,34 @@ package IntroductionProgramming;
 
 import javax.annotation.processing.SupportedSourceVersion;
 import java.sql.Array;
+import java.text.DecimalFormat;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class fifthTask {
+    static Scanner keyboard = new Scanner(System.in);
 
     public static void main(String[] args){
 
 
-        Arrays.stream(fraction(7,3)).forEach(System.out::println);
+        System.out.println("Enter the values, end with q");
+
+        System.out.println(
+                "----------------------------------\n" +
+                "# Test av area och volymmetoderna\n" +
+                "---------------------------------");
+        printAreaVolume();
+
+
+        System.out.println(
+                        "----------------------------------\n" +
+                        "# Test av bråktalsmetoderna\n" +
+                        "----------------------------------");
+
+        printFractionsAll();
+
+
+
     }
 
     public static double area(int radius){
@@ -38,10 +58,19 @@ public class fifthTask {
             return arrValues; //array with only 0
         }
 
+        int commonDivider;
+        int changedNominator;
+        int changedDenominator;
+
+        commonDivider = gcd(nominator, denominator);
+        changedNominator = nominator / commonDivider;
+        changedDenominator = denominator / commonDivider;
+
+
 
         arrValues[0] = nominator/denominator;
-        arrValues[1] = gcd(nominator, denominator);
-        arrValues[2] = denominator;
+        arrValues[1] = changedNominator % changedDenominator;
+        arrValues[2] = changedDenominator;
         return  arrValues;
     }
 
@@ -56,11 +85,106 @@ public class fifthTask {
 
     public static void printFraction(int[] parts){
         String stringOutput = "0";
+        if (parts == null) {
+            System.out.println("Error");
 
-        if(parts[0] == 0){
+        }
+
+        else if(parts[0] == 0 && parts[1] == 0 && parts[2] == 0){
+            System.out.println(stringOutput);
+        }
+
+
+        else if(parts[0] == 0){
             System.out.println(parts[1] +"/"+ parts[2]);
         }
+        else if(parts[1] == 0){
+            System.out.println(parts[0]);
+        }
+        else{
+            //Everything should be alright
+            System.out.println(parts[0] + " " + parts[1] +  "/"+ parts[2]);
+        }
+
     }
+
+
+
+
+    public static void printAreaVolume(){
+        int r = 0;
+        int h  = 0;
+        final DecimalFormat dfSharp = new DecimalFormat("#.##");
+        while(true){
+            r = input();
+            if(r == -1){
+                break;
+            }
+
+            h = input();
+            if(h == -1){
+                break;
+            }
+
+            System.out.println("r = " + r + " h = " + h);
+            System.out.println("Basytans area:      " + dfSharp.format(area(r)));
+            System.out.println("Mantelytans area:   " + dfSharp.format(area(r, h)));
+            System.out.println("volymn:             " + dfSharp.format(volume(r, h)));
+            System.out.println("");
+
+
+        }
+    }
+
+
+    public static void  printFractionsAll(){
+        int nominator = 0;
+        int denominator = 0;
+        final DecimalFormat dfSharp = new DecimalFormat("#.##");
+
+        while(true){
+            nominator = input();
+            if(nominator == -1){
+                break;
+            }
+
+            denominator = input();
+            if(denominator == -1){
+                break;
+            }
+
+            System.out.print(nominator + "/" + denominator +"=      ");
+            printFraction(fraction(nominator, denominator));
+
+        }
+    }
+
+
+
+    public static int input() {
+        String in;
+        int ENDVALUE = -1;
+        String ENDLETTER= "q";
+
+        //Keep going until q, ignore invalid values.
+        while(true) {
+            //Check if it is integer,
+            if(keyboard.hasNextInt()) {
+                return keyboard.nextInt();
+            }
+            else {
+                //Check if it is q
+                in = keyboard.next();
+                if (in.equals(ENDLETTER))
+                    return ENDVALUE;
+                    //Check if it is something else
+               // else {
+                 //   System.out.println(in + " is not a valid integer ");
+                // }
+            }
+        }
+    }
+
 
 
 }
